db68a15bfad500265ed32ef35ba35f7825d87ce1
NathalieLima <nathalielima3@gmail.com>
2023-09-22 10:24:16 -0300

    Adicionando função de obter índice do embarque e adaptações na função de alterarOcorrencia

--- etapa3_alterar.cpp
+++ etapa3_alterar.cpp
@@ -1,9 +1,24 @@
-void alterarOcorrencia(vector<Embarca> &embarques){
+int obterIndiceDoEmbarque(vector<Embarca> &embarques, string cpf, int codigo)
+{
+    int indice_embarque = -1;
+    int i = 0;
+
+    for (Embarca embarque: embarques) {
+        if ( embarque.passageiro.cpf == cpf && embarque.roteiro.codigo == codigo ) {
+            indice_embarque = i;
+        }
+
+        i++;
+    }
+
+    return indice_embarque;
+}
+
+void alterarOcorrencia(vector<Embarca> &embarques)
+{
     int codigo;
     string cpf;
     int indice_embarque;
-    bool encontrou = false;
-    string nomePassageiro;
 
     cout <<endl << "Informe o CPF: ";
     cin.ignore();
@@ -14,76 +29,71 @@ void alterarOcorrencia(vector<Embarca> &embarques){
     cout<<endl;
 
     // localizar o índice do embarque a ser alterado
-    indice_embarque = localizarEmbarque(embarques, cpf, codigo);
+    indice_embarque = obterIndiceDoEmbarque(embarques, cpf, codigo);
 
-    if ( indice_embarque != -1 ) {
-        cout << endl << "Dados Validos!" << endl;
+    if ( indice_embarque != -1 ) 
+    {
+        cout << "Dados Validos!" << endl;
 
-        for (size_t i = 0; i < embarques.size(); i++) {
-            if (embarques[i].passageiro.cpf == cpf && embarques[i].roteiro.codigo == codigo){
-                encontrou = true;
-                nomePassageiro = embarques[i].passageiro.nome;
-                
-                //AlterarOcorrencia
-                string flag;
-                string descricao;
-                string data;
-                int hora;
-                int numApolice;
+        string nomePassageiro = embarques[i].passageiro.nome;
+        
+        //AlterarOcorrencia
+        string flag;
+        string descricao;
+        string data;
+        int hora;
+        int numApolice;
 
-                cout << "Deseja alterar a descricao da ocorrencia (s/n)? ";
-                cin >> flag;
+        cout << "Deseja alterar a descricao da ocorrencia (s/n)? ";
+        cin >> flag;
 
-                if ( flag == "s" ) {
-                    cout <<"Descreva o que aconteceu:" <<endl;
-                    cin.ignore(); 
-                    getline(cin, descricao);
-                } else {
-                    embarques[i].descricao = descricao;
-                }
-                
-                cout << "Deseja alterar a data (s/n)? ";
-                cin >> flag;
-
-                if ( flag == "s" ) {
-                    cout <<"Informe a data: " << endl;
-                    cin.ignore(); 
-                    getline(cin, data);
-                } else {
-                    embarques[i].data = data;
-                }
-
-                cout << "Deseja alterar a hora (s/n)? ";
-                cin >> flag;
-
-                if ( flag == "s" ) {
-                    cout <<"Informe a hora: " << endl;
-                    cin >> hora;
-                } else {
-                    embarques[i].hora = hora;
-                }
-
-                cout << "Deseja alterar o numero do Apolice (s/n)? ";
-                cin >> flag;
-
-                if ( flag == "s" ) {
-                    cout <<"Informe o numero do Apolice: " << endl;
-                    cin >> numApolice;
-                } else {
-                    embarques[i].numApolice = numApolice;
-                }
-
-                cout << "-----------------------------------------------------------------------------" << endl;
-                cout << "\t\tOcorrência de "<<nomePassageiro<<" do Roteiro "<<codigo<<" foi alterada com sucesso." << endl;
-                cout << "-----------------------------------------------------------------------------" << endl;
-            }
+        if ( flag == "s" ) {
+            cout <<"Descreva o que aconteceu:" <<endl;
+            cin.ignore(); 
+            getline(cin, descricao);
+        } else {
+            embarques[indice_embarque].ocorrencia.descricao = descricao;
         }
-        if (!encontrou){
-            cout << "-----------------------------------------------------------------------------" << endl;
-            cout << "\t\tOps! Ocorrência não encontrada." << endl;
-            cout << "-----------------------------------------------------------------------------" << endl;
+        
+        cout << "Deseja alterar a data (s/n)? ";
+        cin >> flag;
+
+        if ( flag == "s" ) {
+            cout <<"Informe a data: " << endl;
+            cin.ignore(); 
+            getline(cin, data);
+        } else {
+            embarques[indice_embarque].ocorrencia.data = data;
         }
-    } else {
-         cout << endl << "Dados Invalidos" << endl;
+
+        cout << "Deseja alterar a hora (s/n)? ";
+        cin >> flag;
+
+        if ( flag == "s" ) {
+            cout <<"Informe a hora: " << endl;
+            cin >> hora;
+        } else {
+            embarques[indice_embarque].ocorrencia.hora = hora;
+        }
+
+        cout << "Deseja alterar o numero do Apolice (s/n)? ";
+        cin >> flag;
+
+        if ( flag == "s" ) {
+            cout <<"Informe o numero do Apolice: " << endl;
+            cin >> numApolice;
+        } else {
+            embarques[indice_embarque].ocorrencia.numApolice = numApolice;
+        }
+
+        cout << "-----------------------------------------------------------------------------" << endl;
+        cout << "\t\tOcorrência de "<<nomePassageiro<<" do Roteiro "<<codigo<<" foi alterada com sucesso." << endl;
+        cout << "-----------------------------------------------------------------------------" << endl;
+    } 
+    else {
+        cout << "Dados Invalidos!" << endl;
+        cout << "-----------------------------------------------------------------------------" << endl;
+        cout << "\t\tOps! Ocorrência não encontrada." << endl;
+        cout << "-----------------------------------------------------------------------------" << endl;
     }
-}
+} 
